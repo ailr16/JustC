4d2c6cd517997147404bd12a7695620bf7d6166b
Angel Lozano <angel_ivan_5@outlook.com>
2024-04-23 23:36:35 -0600

    Finished functions free, print, prepend and append

--- main.c
+++ main.c
@@ -5,15 +5,17 @@ dlist listH;
 int main(void)
 {
     dlist_init(&listH);
+
+    dlist_prepend(&listH, 16);   //[ 16 ]
+    dlist_append(&listH, 32);    //[ 16 32 ]
+    dlist_prepend(&listH, 18);   //[ 18 16 32 ]
+    dlist_append(&listH, 32);    //[ 18 16 32 32 ]
+    dlist_append(&listH, 64);    //[ 18 16 32 32 64 ]
+
+    dlist_print(&listH);
+
+    dlist_free(&listH);
     /*
-    slist_prepend(&listH, 16);   //[ 16 ]
-    slist_append(&listH, 32);    //[ 16 32 ]
-    slist_prepend(&listH, 18);   //[ 18 16 32 ]
-    slist_append(&listH, 32);    //[ 18 16 32 32 ]
-    slist_append(&listH, 64);    //[ 18 16 32 32 64 ]
-
-    slist_print(&listH);
-
     slist_insert(&listH, 0, 99);     //[ 99 18 16 32 32 64 ]
     slist_insert(&listH, 1, 55);     //[ 99 55 18 16 32 32 64 ]
     slist_insert(&listH, 3, 66);     //[ 99 55 18 66 16 32 32 64 ]
@@ -27,8 +29,6 @@ int main(void)
     slist_remove(&listH, 40);        //[ 55 18 16 32 32 64 ]
     slist_remove(&listH, 4);         //[ 55 18 16 32 64 ]
     slist_print(&listH);
-
-    slist_free(&listH);
     */
     return 0;
 }
--- src/dlist.c
+++ src/dlist.c
@@ -26,14 +26,44 @@ void dlist_remove(dlist *listHandler, int index){
   Add node at front of list
 */
 void dlist_prepend(dlist *listHandler, int data){
+    dlist_node *temp;
 
+	temp = malloc(sizeof(dlist_node));
+	temp->data = data;
+	temp->prev = NULL;
+
+	if(listHandler->head == NULL){
+		temp->next = NULL;
+		listHandler->head = temp;
+		listHandler->tail = temp;
+    }
+	else{
+		temp->next = listHandler->head;
+		listHandler->head->prev = temp;
+		listHandler->head = temp;
+	}
 }
 
 /*
   Add node at end of list
 */
 void dlist_append(dlist *listHandler, int data){
+	dlist_node *temp;
 
+	temp = malloc(sizeof(dlist_node));
+	temp->data = data;
+	temp->next = NULL;
+
+	if(listHandler->head == NULL){
+        temp->prev = NULL;
+		listHandler->head = temp;
+		listHandler->tail = temp;
+    }
+	else{
+		temp->prev = listHandler->tail;
+		listHandler->tail->next = temp;
+		listHandler->tail = temp;
+	}
 }
 
 /*
@@ -54,12 +84,27 @@ int  dlist_popFront(dlist *listHandler){
   Printt all nodes in the list
 */
 void dlist_print(dlist *listHandler){
+	dlist_node *temp;
 
+	temp = listHandler->head;
+
+	printf("[ ");
+	while(temp){
+		printf("%d ", temp->data );
+		temp = temp->next;
+	}
+	printf("]\n");
 }
 
 /*
   Remove all nodes and free memory
 */
 void dlist_free(dlist *listHandler){
+	dlist_node * temp;
 
+	while(listHandler->head){
+		temp = listHandler->head;
+		listHandler->head = listHandler->head->next;
+		free(temp);
+	}
 }
