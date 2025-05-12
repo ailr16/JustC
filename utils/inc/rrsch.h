948c88111dee0b1d4668a8ed431729555c7b4bfb
ailr16 <angel_ivan_5@outlook.com>
2025-05-11 20:41:07 -0600

    Added very basic emplate for rr task scheduler

--- utils/inc/rrsch.h
+++ utils/inc/rrsch.h
@@ -1,9 +1,27 @@
 #ifndef RRSCH_H
 #define RRSCH_H
 
-#include <stdlib.h>
-#include <stdio.h>
+typedef enum {
+    OK,
+    ERROR,
+    REACHED_MAX_TASKS,
+    INVALID_TASK_FUNCTION
+} SchedulerReturnT;
 
+typedef struct {
+    const char* taskName;
+    void (*initFunction)(void);
+    void (*taskFunction)(void);
+} TcbT;
 
+typedef struct {
+    int maxTasks;
+    int taskCount;
+    TcbT* taskArray;
+} SchedulerT;
 
+void Scheduler_Init(SchedulerT* schedulerHandler);
+
+SchedulerReturnT Scheduler_RegisterTask(SchedulerT* schedulerHandler, const char* taskName, void* initFunction, void* taskFunction);
+SchedulerReturnT Scheduler_Start(SchedulerT* scheduleHandler);
 #endif
